6bd5b0b08842accac84e216dacf36fb60013ab6f
Cody Garver <codygarver@gmail.com>
2023-07-04 18:35:17 -0400

    Minor clean up

--- leekduck.py
+++ leekduck.py
@@ -16,27 +16,40 @@ page = requests.get(url + "/events/")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Find all event spans
 spans = soup.find_all('span', class_='event-header-item-wrapper')
 
 for span in spans:
-    h2 = span.find('h2')
+    # Create event
     cal_event = ics.Event()
+
+    # Event name
+    h2 = span.find('h2')
     cal_event.name = h2.text
+
+    # Begin date
     begin_date = soup.find("h5", class_="event-header-time-period")["data-event-start-date-check"]
     begin_date_local = datetime.datetime.strptime(begin_date, "%Y-%m-%dT%H:%M:%S%z")
     begin_date_local = begin_date_local.astimezone(pytz.timezone('US/Eastern'))
     # Subtract 12 hours from begin_date_local
     begin_date_local = begin_date_local - datetime.timedelta(hours=12)
     cal_event.begin = begin_date_local
+
+    # End date
     end_date = soup.find("h5", class_="event-header-time-period")["data-event-end-date"]
     end_date_local = datetime.datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S%z")
     end_date_local = end_date_local.astimezone(pytz.timezone('US/Eastern'))
-    cal_event.end = end_date_local
     # Subtract 12 hours from end_date_local
     end_date_local = end_date_local - datetime.timedelta(hours=12)
+    cal_event.end = end_date_local
+
+    # URL
     link = url + span.find('a', class_='event-item-link', href=True)['href']
     cal_event.url = link
+
+    # Add event to calendar
     cal.events.add(cal_event)
 
+# Write calendar to file
 with open('pokemon-go-events.ics', 'w') as cal_file:
     cal_file.writelines(cal)
