8f0d82ebfbaf8d7e33eeb8591b48dfd557bd2475
Cody Garver <codygarver@gmail.com>
2023-07-04 22:09:37 -0400

    Fix timezone

--- leekduck.py
+++ leekduck.py
@@ -21,59 +21,49 @@ soup = BeautifulSoup(page.content, 'html.parser')
 spans = soup.find_all('span', class_='event-header-item-wrapper')
 
 for span in spans:
-    # Create event
-    cal_event = ics.Event()
-
-    # Event name
-    h2 = span.find('h2')
-    cal_event.name = h2.text
-    print("Name: " + cal_event.name)
-
-    # Determine local time
-    if span.find_all("h5", class_="event-header-time-period")[0]["data-event-local-time"] == "true":
-        display_local_time = True
-    else:
-        display_local_time = False
-
-    # Begin date
     try:
-        begin_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-start-date-check"]
-    except:
-        continue
-    # Replace -0800 with -0700
-    #begin_date = begin_date.replace("-0800", "-0700")
-    print("Before: " + begin_date)
-    begin_date_local = datetime.datetime.strptime(begin_date, "%Y-%m-%dT%H:%M:%S%z")
-    if display_local_time:
+        # Create event
+        cal_event = ics.Event()
+
+        # Event name
+        h2 = span.find('h2')
+        cal_event.name = h2.text
+
+        # Determine local time
+        if span.find_all("h5", class_="event-header-time-period")[0]["data-event-local-time"] == "true":
+            display_local_time = True
+        else:
+            display_local_time = False
+
+        # Begin date
+        if display_local_time:
+            begin_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-start-date-check"][:-5] + "-0400"
+        else:
+            begin_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-start-date-check"]
+        begin_date_local = datetime.datetime.strptime(begin_date, "%Y-%m-%dT%H:%M:%S%z")
         begin_date_local = begin_date_local.astimezone(pytz.timezone('US/Eastern'))
-    else:
-        begin_date_local = begin_date_local.astimezone(pytz.timezone('UTC'))
-    print(begin_date_local)
-    cal_event.begin = begin_date_local
+        cal_event.begin = begin_date_local
 
-    # End date
-    try:
-        end_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-end-date"]
+        # End date
+        if display_local_time:
+            end_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-end-date"][:-5] + "-0400"
+        else:
+            end_date = span.find_all("h5", class_="event-header-time-period")[0]["data-event-end-date"]
+        end_date_local = datetime.datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S%z")
+        end_date_local = end_date_local.astimezone(pytz.timezone('US/Eastern'))
+        cal_event.end = end_date_local
+
+        # URL
+        link = url + span.find('a', class_='event-item-link', href=True)['href']
+        cal_event.url = link
+
+        # DTSTAMP
+        cal_event.created = begin_date_local
+
+        # Add event to calendar
+        cal.events.add(cal_event)
     except:
         continue
-    end_date_local = datetime.datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S%z")
-    print(end_date_local)
-    if display_local_time:
-        end_date_local = end_date_local.astimezone(pytz.timezone('US/Eastern'))
-    else:
-        end_date_local = end_date_local.astimezone(pytz.timezone('UTC'))
-    print(end_date_local)
-    cal_event.end = end_date_local
-
-    # URL
-    link = url + span.find('a', class_='event-item-link', href=True)['href']
-    cal_event.url = link
-
-    # DTSTAMP
-    cal_event.created = begin_date_local
-
-    # Add event to calendar
-    cal.events.add(cal_event)
 
 # Write calendar to file
 with open('pokemon-go-events.ics', 'w') as cal_file:
