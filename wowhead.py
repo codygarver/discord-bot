074b125e7032bb2fc9a6650feb3980a49748be02
Cody Garver <codygarver@gmail.com>
2024-08-27 16:28:01 -0400

    wowhead: ignore twiw

--- wowhead.py
+++ wowhead.py
@@ -95,6 +95,9 @@ if __name__ == "__main__":
         title = get_title(item)
         title_lower = title.strip().lower()
 
+        if "twiw" in title_lower:
+            continue
+
         if "prime gaming" not in title_lower and "twitch drop" not in title_lower:
             continue
 
